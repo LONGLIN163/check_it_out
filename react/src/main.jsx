d718acee95b0017400119a107e3dd1c5453e94ca
linlong <developerlin123@gmail.com>
2023-06-27 17:40:07 +0200

    create use context api

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router-dom";
 import router from './router';
+import { UserContextProvider } from './store/ContextProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
   </React.StrictMode>,
 )
---
+++ react/src/store/ContextProvider.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext=createContext({
+  currentUser:{},
+  setcurrentUser:()=>{},
+  userToken:null,
+  setUserToken:()=>{}
+});
+
+export const UserContextProvider= ({children}) => {
+ 
+   const [currentUser, setCurrentUser] = useState({
+      name: 'Tom Cook',
+      email: 'tom@example.com'
+   });
+   const [userToken, setUserToken] = useState(null);
+    
+  return (
+    <UserContext.Provider value={{
+      currentUser, setCurrentUser,
+      userToken, setUserToken
+    }}>
+        {children}
+
+    </UserContext.Provider>
+  )
+}
+
+export const UserStateContext=()=>useContext(UserContext);
