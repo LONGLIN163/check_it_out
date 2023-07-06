5df089801ef8a4b62dad7b6e86487a8bb789140c
linlong <developerlin123@gmail.com>
2023-07-06 15:07:22 +0200

    add login and sign route and details of AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+    public function signup(SignupRequest $request){
+
+        /** @var \App\Models\User $user */
+
+        $data=$request->validated();
+
+        $user=User::create([
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>bcrypt($data['password']),
+        ]);
+
+        $token=$user->createToken('haha')->plainTextToken;
+
+        return response([
+            'user'=>$user,
+            'token'=>$token
+        ]);
+    }
+
+    public function login(LoginRequest $request){
+        $credentials=$request->validated();
+        $remember=$credentials['remember']??false; // if not exist, we give this prop false
+        unset($credentials['remember']);
+
+        if(!Auth::attempt($credentials,$remember)){ // if not logged in, then ....
+            return response([
+                'error'=>"the provided credential is not correct"
+            ],422);
+        };
+
+        // if logged in,....
+        $user=Auth::user();
+        $token=$user->createToken('haha')->plainTextToken;
+
+        return response([
+            'user'=>$user,
+            'token'=>$token
+        ]);
+    }
+    
+    public function logout(Request $request){
+
+        /** @var User $user */
+        
+        $user=Auth::user();
+        $user->currentAccessToken()->delete();
+
+        return response([
+            'success'=>true,
+        ]);
+        
+    }
+}
