8ec686b1d85fe0eb883ac017597f4af67182dfa3
linlong <developerlin123@gmail.com>
2023-08-30 22:13:03 +0200

    login and save logged in status

--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -1,9 +1,49 @@
 import { LockClosedIcon } from '@heroicons/react/24/solid'
+import axiosClient from './axios.js';
+import { useState } from 'react';
+import { UserStateContext } from '../store/ContextProvider.jsx';
+
+
 
 const Login = () => {
+
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+
+  const [error,setError]=useState({__html:''});
+  const {setUserToken,setCurrentUser}=UserStateContext();
+
+
+  const onSubmit= (ev) => {
+    ev.preventDefault();
+    setError({__html:''});
+
+    axiosClient
+    .post('/login',{
+      email:email,
+      password:password,
+    })
+    .then(({data}) => {
+      console.log(data)
+      setCurrentUser(data.user);
+      setUserToken(data.token);
+    })
+    .catch((error)=>{
+      console.log(error)
+      if(error.response){
+        const errorsArr=Object.values(error.response.data.errors).reduce(
+          (accum,next) => [...accum,...next],[]  
+        )
+        console.log(errorsArr)
+        setError({__html:errorsArr.join('<br />')})
+      }
+      console.error(error)
+    })
+  }
+
     return (  
       <>
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div onSubmit={onSubmit} className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
@@ -16,6 +56,8 @@ const Login = () => {
                     type="email"
                     autoComplete="email"
                     required
+                    value={email}
+                    onChange={ ev => setEmail(ev.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -39,21 +81,32 @@ const Login = () => {
                     type="password"
                     autoComplete="current-password"
                     required
+                    value={password}
+                    onChange={ ev => setPassword(ev.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
-  
+
               <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
+                  >
                     <LockClosedIcon className="h-5 w-5"/>
                   Sign in
                 </button>
               </div>
             </form>
+
+            {error.__html && (
+              <div 
+                className='bg-red-500 rounded my-5 px-3 py-1.5 text-white'
+                dangerouslySetInnerHTML={error}
+              >
+              </div>
+            )}
+
         </div>
       </>
     );
